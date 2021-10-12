8f2d39563ee2d8de78533cb6428484a0d06e2abb
Esneyder Saavedra <esneydersa@hotmail.com>
2021-10-11 19:30:01 -0500

    instalando expres y creando extrutura de archivos básica

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+// definir que archivos son publicos
+const publicPath =path.resolve(__dirname,'public');
+app.use(express.static(publicPath));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./vievs/index.html'))
+})
+
+app.listen(port, () => console.log('listen in port 3000'))
