231ef61bc3e6f0757bd4108c7576e044bbb677b2
Esneyder Saavedra <esneydersa@hotmail.com>
2021-10-23 12:13:16 -0500

    Agregando seccion login,incluyendo diseño responsive

--- app.js
+++ app.js
@@ -19,4 +19,7 @@ app.get('/register',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
 })
 
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
 app.listen(port, () => console.log('listen in port 3000'))
