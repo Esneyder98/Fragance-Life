53eed3e9362b52acc74d5ac79570870cebcc1d34
NickoNO242 <nico568_97@hotmail.com>
2021-11-15 10:32:55 -0500

    creacion de archivos JSON

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,8 @@
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const controller ={
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const controller ={
 
 
