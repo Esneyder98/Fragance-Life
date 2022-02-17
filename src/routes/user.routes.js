6a4c2dd5b87d18ccec48fb8b36830579b08799f9
Cristianoz <mayteoelan@gmail.com>
2022-02-17 16:20:47 -0500

    Adicion de Middlewares creados en user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,25 +12,17 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const adminMiddleware = require('../middlewares/adminMiddleware');
 const buyerMiddleware = require('../middlewares/buyerMiddleware');
 const userLoggedMiddleware = require('../middlewares/userLoggedMiddleware');
-
-const validaciones = [
-    body('document').notEmpty().withMessage('¡Debes ingresar un numero de documento!'),
-    body('names').notEmpty().withMessage('¡Debes ingresar al menos un nombre!'),
-    body('surnames').notEmpty().withMessage('¡Debes ingresar al menos un apellido!'),
-    body('email').isEmail().withMessage('¡Debes ingresar un email valido!'),
-    body('password').notEmpty().withMessage('¡Debes ingresar una contraseña!'),
-    body('confirmpassword').notEmpty().withMessage('¡Debes ingresar una contraseña!'),
-    body('category').notEmpty().withMessage('¡Debes ingresar una categoria!')
-];
+const validationLogin = require('../middlewares/validationLogin');
+const validationRegister = require('../middlewares/validationRegister')
 
 const userController = require('../controllers/user.controller');
 
 
 router.get('/register',guestMideleware, userController.register);
-router.post('/register', uploadFile.single('avatar'), validaciones, userController.processRegister);
+router.post('/register', uploadFile.single('avatar'), validationRegister, userController.processRegister);
 
 router.get('/login',guestMideleware, userController.login);
-router.post('/login', userController.loginProcess);
+router.post('/login', validationLogin , userController.loginProcess);
 
 // Perfil de Usuario
 router.get('/profile',authMiddleware, userController.profile);
