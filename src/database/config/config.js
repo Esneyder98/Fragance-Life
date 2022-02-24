3fdc293f09e738e1a6a1630577208c3f5e41dce4
NickoNO242 <nico568_97@hotmail.com>
2022-02-23 19:20:17 -0500

    prueba index

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,5 +1,11 @@
 require("dotenv").config()
 const env = process.env
+let USER = env.USER
+let DATABASE_PASSWORD = env.DATABASE_PASSWORD
+let DATABASE_NAME = env.DATABASE_NAME
+let DATABASE_HOST = env.DATABASE_HOST
+let DATABASE_PORT = env.DATABASE_PORT
+
 module.exports = {
 	development: {
 		username: env.USER,
@@ -17,10 +23,10 @@ module.exports = {
 		dialect: 'mysql',
 	},
 	production: {
-		username: env.USER,
-		password: env.DATABASE_PASSWORD,
-		database: env.DATABASE_NAME,
-		host: env.DATABASE_HOST,
+		username: USER,
+		password: DATABASE_PASSWORD,
+		database: DATABASE_NAME,
+		host: DATABASE_HOST,
 		dialect: 'mysql',
 	},
 	}
