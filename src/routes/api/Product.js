fa012f86ea0350e3c26dbd678c8a18c385e967db
NickoNO242 <nico568_97@hotmail.com>
2022-02-25 13:26:34 -0500

    cors

--- src/routes/api/Product.js
+++ src/routes/api/Product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const productAPIController = require('../../controllers/api/productAPIController');
 
 //Rutas
 
-router.get('/', productAPIController.list);
+router.get('/', cors(), productAPIController.list);
 
 //detalle de un producto
-router.get('/:id', productAPIController.detail);
+router.get('/:id', cors(), productAPIController.detail);
 
 module.exports = router;
