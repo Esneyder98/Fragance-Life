e687b518cd686f686cb0cbc53c7eebbbe3f64721
Cristianoz <mayteoelan@gmail.com>
2022-02-25 10:20:34 -0500

    Se realiza api de productos

--- src/controllers/api/productAPIController.js
+++ src/controllers/api/productAPIController.js
@@ -7,9 +7,52 @@ const { Op } = require("sequelize");
 
 const productAPIController = {
     'list': (req, res) => {
-        res.send("hola mundo")
+        db.products.findAll({
+            include: ['brand']
+        })
+        .then(products => {
+            function brand(prods){
+                let ids={}
+                for (let i = 0; i < prods.length; i++) {
+                    let element =  prods[i].brand.name;
+                    ids[element] = 0
+                }
+                for (let i = 0; i < prods.length; i++) {
+                    let element =  prods[i].brand.name;
+                    ids[element]+=1
+                }
+                    return ids
+            }
+            let respuesta = {
+                meta: {
+                    status : 200,
+                    count : products.length,
+                    countBrands : brand(products),
+                    url: 'api/products'
+                },
+                data: products
+            }
+                res.json(respuesta);
+            })
     },
-    
+    'detail': (req, res) => {
+        db.products.findByPk(req.params.id,
+            {include: ['brand','smellFamily','images_products']})
+            .then(product => {
+                let protocol= req.protocol
+                let hosname= req.hostname
+                let host= req.headers.host
+                 let img=protocol+'://'+host+'/img/Perfumes/'+product.images_products[0].name
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        url: '/api/products/:id'
+                    },
+                    data: product,img
+                }
+                res.json(respuesta);
+            });
+    }
     
 }
 
