3455876f266498692b979efcdb592c42dcd0eec4
Gerarck2021 <gaarciniegas23@misena.edu.co>
2022-02-27 02:55:24 -0500

    Se coloca API de paginacion de usuarios en las rutas de usuario

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const userAPIController = require('../../controllers/api/userAPIController');
+const paginationUserController = require('../../controllers/api/paginationUserController');
 
 //Rutas
 
 router.get('/', cors(), userAPIController.list);
 //Detalle de una usuario
 router.get('/:id', cors(), userAPIController.detail)
+
+//paginacion de usuarios
+router.get('/paginationUsers/:page', paginationUserController.paginationUsers);
 
 module.exports = router;
