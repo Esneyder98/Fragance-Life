2e95c894f3db900024006e8e3743b6c10228a250
Cristianoz <mayteoelan@gmail.com>
2022-02-27 02:47:18 -0500

    Se implementa API de paginacion en el archivo de rutas de productos

--- src/routes/api/Product.js
+++ src/routes/api/Product.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const productAPIController = require('../../controllers/api/productAPIController');
-
+const paginationProductController = require('../../controllers/api/paginationProductController');
 //Rutas
 
 router.get('/', cors(), productAPIController.list);
 
 //detalle de un producto
 router.get('/:id', cors(), productAPIController.detail);
+
+//paginacion de productos:
+router.get('/paginationProducts/:page', cors(),paginationProductController.paginationProducts);
 
 module.exports = router;
